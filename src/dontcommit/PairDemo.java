400b07a2fcd2635fd57f7a73eea24d82e5e81853
Arjun Thakur <thakurarjun247@gmail.com>
2022-08-31 15:54:16 +0530

    First Java Program

---
+++ src/dontcommit/PairDemo.java
@@ -0,0 +1,9 @@
+package dontcommit;
+
+import com.mysql.cj.conf.ConnectionUrlParser;
+
+public class PairDemo {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/ja111/web20/day1/Hello.java
@@ -0,0 +1,28 @@
+package ja111.web20.day1;
+
+public class Hello {
+
+    /*
+    *
+    *
+    * */
+
+    //signature
+    public static void main(String[] stringArray) {
+        //it will insert empty line before the next statement.
+        System.out.println("Hello Java");
+        System.out.println(90);
+        multiplication();
+        System.out.println("back to main");
+
+    }
+
+    static void multiplication(){
+        System.out.println("inside the multiplication method");
+        int a=10;
+        int y=7;
+        System.out.println(a*y);
+
+    }
+}
+//shortcut to (un)comment: control+shift+/
