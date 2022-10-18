129161eb1e980159cfd2c615ddd15a4c4db7715b
Arjun Thakur <thakurarjun247@gmail.com>
2022-10-18 15:19:17 +0530

    final

--- src/ja111/web20z/day7/_4instanceOf.java
+++ src/ja111/web20z/day7/_4instanceOf.java
@@ -4,7 +4,31 @@ package ja111.web20z.day7;
 
 public class _4instanceOf {
     public static void main(String[] args) {
+        Animal animal= new Animal();
+        takeAnimal(animal);
 
+        Elephant elephant= new Elephant();
+        takeAnimal(elephant);
+
+        Fish fish= new Fish();
+        takeAnimal(fish);
+    }
+
+    static void takeAnimal(Animal animal){
+        animal.speak();
+        //animal.swim(); //you can call only the methods that are present in animal
+       // animal.swim();
+        //Fish fish= new Fish();
+       // fish.swim();
+
+        if( animal instanceof Fish){
+
+            byte b=10;
+            int i=b;
+            byte anotherByte=(byte) i;
+            Fish f1=(Fish) animal;
+            f1.swim();
+        }
     }
     //write a static method
     //takeAnimal(Animal animal)
@@ -15,8 +39,12 @@ class Animal{
     void speak(){
         System.out.println("animal speaks");
     }
+
 }
-class Elephant extends Animal{}
+
+
+
+class Elephant extends Animal{} //elep. is-a animal
 
 class Fish extends Animal{
     void swim(){
---
+++ src/ja111/web20z/day7/_5final.java
@@ -0,0 +1,42 @@
+package ja111.web20z.day7;
+
+public class _5final {
+
+
+    public static void main(String[] args) {
+        //what is final
+        //what is diff final, finally, finalize
+
+        //final is a keyword
+        //variable: unchanged, no reassignment
+        //method: can't overridden
+        //class: can't extend it
+        final int x=10;
+       // x++; //just like a string
+       // x=20; //no reassignment
+        final Double pi=3.14;
+        //pi+=34.9;
+    }
+}
+class Abc{
+    final void hi(){ //legal Contract
+        System.out.println("hi");
+    }
+
+
+}
+class Bcd extends Abc{
+   // @Override
+    /*void hi() {
+
+    }*/
+
+
+}
+
+abstract class X{
+    //Illegal combination of modifiers: 'abstract' and 'final'
+   //final abstract void hello();
+   //final: can't override
+    //abstract: must override
+}
