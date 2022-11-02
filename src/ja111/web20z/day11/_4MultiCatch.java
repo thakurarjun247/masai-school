e1a171a2c939d613f5e71c07f57dfd413b81fa00
Arjun Thakur <thakurarjun247@gmail.com>
2022-11-02 15:42:43 +0530

    nested try catch

--- src/ja111/web20z/day11/_4MultiCatch.java
+++ src/ja111/web20z/day11/_4MultiCatch.java
@@ -1,22 +1,29 @@
 package ja111.web20z.day11;
 
+import ja111.web20.day14.Plant;
+
 public class _4MultiCatch {
     public static void main(String[] args) {
         try{
+            Plant o=null;
+            System.out.println(o.toString());
             int a[]=new int[5];
-            a[6]=30/0;
+            a[6]= 56;
+            a[2]=  30/0;
             System.out.println("End of try");
         }
         catch (ArrayIndexOutOfBoundsException exception){
             System.out.println("Array Index OOB Exception ");
         }
         catch (ArithmeticException arithmeticException){
-            //....
+            System.out.println("ArithmeticException Exception ");
         }
         catch (Exception exception){
-
+            System.out.println("generic Exception");
         }
 
         System.out.println("rest of the code");
     }
+
+    //resume recording...
 }
---
+++ src/ja111/web20z/day11/_5NestedTryCatch.java
@@ -0,0 +1,50 @@
+package ja111.web20z.day11;
+
+public class _5NestedTryCatch {
+}
+ class Main{
+
+    public static void main(String args[]){
+
+        try {  // outer (main) try block
+
+            try {  //inner try block 1
+
+                try {  // inner try block 2
+                    int arr[] = { 1, 2, 3, 4 };
+
+                    //printing the array element out of its bounds
+                    System.out.println(arr[10]);
+                }
+
+                // to handles ArithmeticException
+                catch (ArithmeticException e) {
+                    System.out.println("Arithmetic exception");
+                    System.out.println(" inner catch block 2");
+                }
+            }
+
+            // to handle ArithmeticException
+            catch (ArithmeticException e) {
+                System.out.println("Arithmetic exception");
+                System.out.println("inner catch block 1");
+            }
+        }
+
+        // to handle ArrayIndexOutOfBoundsException
+        catch (ArrayIndexOutOfBoundsException e4) {
+            System.out.println(e4);
+            System.out.println(" outer (main) try block");
+        }
+        catch (Exception e5) {
+            System.out.println("Exception");
+            System.out.println(" handled in main try-block");
+        }
+
+    }
+}
+
+
+
+
+
